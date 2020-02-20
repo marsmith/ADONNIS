0bee894362534e5aa7c76370038c2e40510d0086
Scilipoti <iscilipoti@contractor.usgs.gov>
2020-02-20 13:24:11 -0500

    query using requests now works. Number of query attempts can be input into the query from web function as sometimes it fails on the first try. Special treatment had to be given to the site query since the query results are xml and needed to be geojson to get imported into gdal

--- backEnd/GDALData.py
+++ backEnd/GDALData.py
@@ -7,6 +7,9 @@ from osgeo import gdal_array
 from osgeo import gdalconst
 import requests
 import os
+from collections import namedtuple
+import xml.etree.ElementTree as ET
+import json
 
 #This class provides a generic way of passing around data
 #New instances of GDALData can be generated either from online query or via local data storage. 
@@ -35,12 +38,90 @@ class GDALData(object):
         # this reference is likely required for things like siteLayer.GetSpatialRef()
         self.siteLayer = self.siteDataSource.GetLayer()
 
-    def loadFromQuery(self, lat, lng):
-        lineURL = "https://hydro.nationalmap.gov/arcgis/rest/services/nhd/MapServer/6/query?geometry=" + str(lat) + "," + str(lng) + "&outFields=GNIS_NAME%2CREACHCODE&geometryType=esriGeometryPoint&inSR=4326&outSR=4326&distance=4000&units=esriSRUnit_Meter&outFields=*&returnGeometry=true&f=geojson"
-        req = requests.get(lineURL)
+    def approxKmToDegrees (self, km):
+        return (1/111) * km
+
+    def buildGeoJson (self, xmlStr):
+        root = ET.fromstring(xmlStr)
+        geojson = {
+            "type": "FeatureCollection",
+            "crs": {"type":"name","properties":{"name":"EPSG:4326"}},
+            "features":[]
+        }
+        for site in root:
+            siteNo = int(site.find('site_no').text)
+            stationNm = site.find('station_nm').text
+            siteType = site.find('site_tp_cd').text
+            siteLat = float(site.find('dec_lat_va').text)
+            siteLng = float(site.find('dec_long_va').text)
+            #make sure this is a stream not a well
+            if(siteType == "ST"):
+                #build feature. This is based on the format of the geoJson returned from the streams query
+                feature = {
+                    "type":"Feature",
+                    "geometry":{
+                        "type": "Point",
+                        "coordinates": [siteLat, siteLng]
+                    },
+                    "properties": {
+                        "site_no":siteNo,
+                        "station_nm":stationNm
+                    }
+                }
+                geojson["features"].append(feature)
+        return json.dumps(geojson)
+
+    def loadFromQuery(self, lat, lng, maxRequestAttempts):
+        lineURL = "https://hydro.nationalmap.gov/arcgis/rest/services/nhd/MapServer/6/query?geometry=" + str(lng) + "," + str(lat) + "&outFields=GNIS_NAME%2CREACHCODE&geometryType=esriGeometryPoint&inSR=4326&outSR=4326&distance=4000&units=esriSRUnit_Meter&outFields=*&returnGeometry=true&f=geojson"
+        print("requesting stream data...")
+        lineDataAttempts = 0
+        success = False
+        while (lineDataAttempts < maxRequestAttempts):
+            try:
+                req = requests.get(lineURL, timeout=5)
+                success = True
+                break
+            except requests.exceptions.ReadTimeout:
+                print("failed to retrieve stream data on attempt " + str(lineDataAttempts) + ". Trying again")
+        if success == False:
+            print("failed to retrieve stream data on all attempts. Failing")
+            return None
+        
+        print("recieved stream data!")
         self.lineDataSource = gdal.OpenEx(req.text)
         self.lineLayer = self.lineDataSource.GetLayer()
-        #for site in layer:
-          #  print(site)
-        #fill this in with query stuff
+
+        radius = 5 #km this value is defined in gdalData somewhere
+        approxRadiusInDeg = self.approxKmToDegrees(radius)
+
+        #northwest
+        nwLat = lat + approxRadiusInDeg
+        nwLng = lng + approxRadiusInDeg
+
+        #southeast
+        seLat = lat - approxRadiusInDeg
+        seLng = lng - approxRadiusInDeg
+
+        print("requesting gauge site data...")
+        siteURL = "https://waterdata.usgs.gov/nwis/inventory?nw_longitude_va=" + str(nwLng) + "&nw_latitude_va=" + str(nwLat) + "&se_longitude_va=" + str(seLng) + "&se_latitude_va=" + str(seLat) + "&coordinate_format=decimal_degrees&group_key=NONE&format=sitefile_output&sitefile_output_format=xml&column_name=site_no&column_name=station_nm&column_name=site_tp_cd&column_name=dec_lat_va&column_name=dec_long_va&list_of_search_criteria=lat_long_bounding_box"
+        
+        siteDataAttempts = 0
+        success = False
+        while (siteDataAttempts < maxRequestAttempts):
+            try:
+                req = requests.get(siteURL, timeout=5)
+                success = True
+                break
+            except requests.exceptions.ReadTimeout:
+                print("failed to retrieve site data on attempt " + str(siteDataAttempts) + ". Trying again")
+        if success == False:
+            print("failed to retrieve site data on all attempts. Failing")
+            return None
+        
+        print("recieved gage data! converting to geoJSON...")
+        xmlSites = req.text#this query returns an xml. Have to conver to geoJson
+        geoJsonSites = self.buildGeoJson(xmlSites)
+        self.siteDataSource = gdal.OpenEx(geoJsonSites)
+        self.siteLayer = self.siteDataSource.GetLayer()
+        print("stream data successfully recieved.")
 
