db4d002af273448602b5c4d31a40b74a6c43b7f8
Scilipoti <iscilipoti@contractor.usgs.gov>
2020-02-21 16:39:23 -0500

    query requests now return projected data. Implimentation is not ideal. The geojson files are modified directly to transform coordinates. Theoretically, this seems like something gdal can do but getting it to write to a datasource seems messy

--- backEnd/GDALData.py
+++ backEnd/GDALData.py
@@ -10,6 +10,7 @@ import os
 from collections import namedtuple
 import xml.etree.ElementTree as ET
 import json
+import math
 
 #This class provides a generic way of passing around data
 #New instances of GDALData can be generated either from online query or via local data storage. 
@@ -41,11 +42,31 @@ class GDALData(object):
     def approxKmToDegrees (self, km):
         return (1/111) * km
 
-    def buildGeoJson (self, xmlStr):
+    def getUTM (self, long):
+        return (math.floor((long + 180)/6) % 60) + 1
+
+    def queryWithAttempts (self, url, attempts, timeout = 3, queryName="data"):
+        attemptsUsed = 0
+        success = False
+        while (attemptsUsed < attempts):
+            try:
+                req = requests.get(url, timeout=timeout)
+                success = True
+                print("queried " + queryName + " successfully!")
+                return req
+            except requests.exceptions.ReadTimeout:
+                attemptsUsed += 1
+                print("failed to retrieve " + queryName + " on attempt " + str(attemptsUsed) + ". Trying again")
+        if success == False:
+            print("failed to retrieve " + queryName + " on all attempts. Failing")
+            return None
+
+    def buildGeoJson (self, xmlStr, EPSGCode, transformation):
+      
         root = ET.fromstring(xmlStr)
         geojson = {
             "type": "FeatureCollection",
-            "crs": {"type":"name","properties":{"name":"EPSG:4326"}},
+            "crs": {"type":"name","properties":{"name":"EPSG:" + str(EPSGCode)}},
             "features":[]
         }
         for site in root:
@@ -54,6 +75,9 @@ class GDALData(object):
             siteType = site.find('site_tp_cd').text
             siteLat = float(site.find('dec_lat_va').text)
             siteLng = float(site.find('dec_long_va').text)
+
+            [projX, projY, z] = transformation.TransformPoint(siteLng, siteLat)
+
             #make sure this is a stream not a well
             if(siteType == "ST"):
                 #build feature. This is based on the format of the geoJson returned from the streams query
@@ -61,7 +85,7 @@ class GDALData(object):
                     "type":"Feature",
                     "geometry":{
                         "type": "Point",
-                        "coordinates": [siteLat, siteLng]
+                        "coordinates": [projX, projY]
                     },
                     "properties": {
                         "site_no":siteNo,
@@ -72,26 +96,45 @@ class GDALData(object):
         return json.dumps(geojson)
 
     def loadFromQuery(self, lat, lng, maxRequestAttempts):
-        lineURL = "https://hydro.nationalmap.gov/arcgis/rest/services/nhd/MapServer/6/query?geometry=" + str(lng) + "," + str(lat) + "&outFields=GNIS_NAME%2CREACHCODE&geometryType=esriGeometryPoint&inSR=4326&outSR=4326&distance=4000&units=esriSRUnit_Meter&outFields=*&returnGeometry=true&f=geojson"
-        print("requesting stream data...")
-        lineDataAttempts = 0
-        success = False
-        while (lineDataAttempts < maxRequestAttempts):
-            try:
-                req = requests.get(lineURL, timeout=5)
-                success = True
-                break
-            except requests.exceptions.ReadTimeout:
-                print("failed to retrieve stream data on attempt " + str(lineDataAttempts) + ". Trying again")
-        if success == False:
-            print("failed to retrieve stream data on all attempts. Failing")
-            return None
+        #Get UTM and EPSG codes to convert coordinates to projection
+
+        utmCode = self.getUTM(lng)
+        EPSGCode = int("269" + str(utmCode))
+
+        worldRef = osr.SpatialReference()
+        worldRef.ImportFromEPSG(4326)
+
+        stateRef = osr.SpatialReference()
+        stateRef.ImportFromEPSG(EPSGCode) #projection from UTM zone of the current lat/lng
+        print("utm is " + str(utmCode))
+        transformation = osr.CoordinateTransformation(worldRef,stateRef)
+
+        lineURL = "https://hydro.nationalmap.gov/arcgis/rest/services/nhd/MapServer/6/query?geometry=" + str(lng) + "," + str(lat) + "&outFields=GNIS_NAME%2CREACHCODE&geometryType=esriGeometryPoint&inSR=4326&outSR=4326&distance=10000&units=esriSRUnit_Meter&outFields=*&returnGeometry=true&f=geojson"        
+        req = self.queryWithAttempts(lineURL, 4, queryName="lineData")
+        lineJson = json.loads(req.text)
+        #change coordinates to UTM projection. Start with changing the metadata
+        lineJson["crs"]["properties"]["name"] = "EPSG" + str(EPSGCode)
+        for feature in lineJson["features"]:
+            for coords in feature["geometry"]["coordinates"]:
+                featureLat = coords[1]
+                featureLng = coords[0]
+                [projX, projY, z] = transformation.TransformPoint(featureLng, featureLat)
+                coords[0] = projX
+                coords[1] = projY
+
+        projectedJson = json.dumps(lineJson)
+        print(projectedJson[0:300])
         
-        print("recieved stream data!")
-        self.lineDataSource = gdal.OpenEx(req.text)
+        self.lineDataSource = gdal.OpenEx(projectedJson)
         self.lineLayer = self.lineDataSource.GetLayer()
 
-        radius = 5 #km this value is defined in gdalData somewhere
+        #transform geometry to utm projection
+        self.lineLayer.ResetReading()
+        for line in self.lineLayer:
+            geomRef = line.GetGeometryRef()
+            geomRef.Transform(transformation)
+
+        radius = 10 #km this value is defined in gdalData somewhere
         approxRadiusInDeg = self.approxKmToDegrees(radius)
 
         #northwest
@@ -102,26 +145,19 @@ class GDALData(object):
         seLat = lat - approxRadiusInDeg
         seLng = lng - approxRadiusInDeg
 
-        print("requesting gauge site data...")
         siteURL = "https://waterdata.usgs.gov/nwis/inventory?nw_longitude_va=" + str(nwLng) + "&nw_latitude_va=" + str(nwLat) + "&se_longitude_va=" + str(seLng) + "&se_latitude_va=" + str(seLat) + "&coordinate_format=decimal_degrees&group_key=NONE&format=sitefile_output&sitefile_output_format=xml&column_name=site_no&column_name=station_nm&column_name=site_tp_cd&column_name=dec_lat_va&column_name=dec_long_va&list_of_search_criteria=lat_long_bounding_box"
-        
-        siteDataAttempts = 0
-        success = False
-        while (siteDataAttempts < maxRequestAttempts):
-            try:
-                req = requests.get(siteURL, timeout=5)
-                success = True
-                break
-            except requests.exceptions.ReadTimeout:
-                print("failed to retrieve site data on attempt " + str(siteDataAttempts) + ". Trying again")
-        if success == False:
-            print("failed to retrieve site data on all attempts. Failing")
-            return None
-        
-        print("recieved gage data! converting to geoJSON...")
+        req = req = self.queryWithAttempts(siteURL, 4, queryName="siteData")
+
         xmlSites = req.text#this query returns an xml. Have to conver to geoJson
-        geoJsonSites = self.buildGeoJson(xmlSites)
+        geoJsonSites = self.buildGeoJson(xmlSites, EPSGCode, transformation)
         self.siteDataSource = gdal.OpenEx(geoJsonSites)
         self.siteLayer = self.siteDataSource.GetLayer()
-        print("stream data successfully recieved.")
+
+        self.siteLayer.ResetReading()
+        
+        print(self.siteLayer.GetSpatialRef())
+        
+
+
+        print(EPSGCode)
 
