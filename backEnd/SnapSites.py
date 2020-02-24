63d96481c445cd33e29dea67749336c176df596f
Scilipoti <iscilipoti@contractor.usgs.gov>
2020-02-24 14:27:39 -0500

    WIP of snap sites. Added some functionality to attempt to extract the stream name from the site name

--- backEnd/SnapSites.py
+++ backEnd/SnapSites.py
@@ -1,5 +1,7 @@
 from GDALData import *
 import matplotlib.pyplot as plt
+import math
+import sys
 """ 
 class SnapSite(object):
     def __init__(self, maxSnapDist):
@@ -7,33 +9,93 @@ class SnapSite(object):
         self.maxSnapDist = maxSnapDist
  """
 
-maxSnapDist = 3
+maxSnapDist = 1000
+snapVerificationTolerance = 0.2#if the second nearest snap location is within 20% of the distance of the nearest, raise a warning flag
+numSecondarySnapsConsidered = 4# how many possible locations that aren't the same as the nearest do we consider?
+adverbNameSeparators = [" at ", " above ", " near "]
+waterTypeNames = [" brook", " pond", " river", " lake", " stream", " outlet", " creek", " bk", " ck"]
+
+#Gets a key string from the site name that could be used to help snap sites later 
+def getSiteStreamNameIdentifier (siteName):
+    lowerCase = siteName.lower()
+    endIndex = len(siteName)-1
+    for adverb in adverbNameSeparators:
+        try:
+            endIndex = lowerCase.index(adverb)  
+            break  
+        except:       
+            pass
+    split = lowerCase[0:endIndex]
+    
+    for waterBody in waterTypeNames:
+        try:
+            endIndex = split.index(waterBody)
+        except:
+            pass
+
+    if endIndex == len(siteName)-1:
+        return ""
+    return siteName[0:endIndex]
+
 def Snap(gdalData):
     print("snap")
     siteLayer = gdalData.siteLayer
     lineLayer = gdalData.lineLayer
     siteLayer.ResetReading()
+    sideInd = 0
     for site in siteLayer:
         lineLayer.ResetReading()
 
-        geomRef = site.GetGeometryRef()
-        buffer = geomRef.Buffer(maxSnapDist)
-        test1 = 0
-        for line in lineLayer:
-            test1 += 1
+        siteGeom = site.GetGeometryRef()
+        buffer = siteGeom.Buffer(maxSnapDist)
         lineLayer.SetSpatialFilter(buffer)
-        test2 = 0
+        potentialLines = []
         for line in lineLayer:
-            test2 += 1
+            potentialLines.append(line)
         
-        #print("test1 = " + str(test1))
-        #print(test2)
+        sitePoint = siteGeom.GetPoint(0)
+
+        #for all segments, store the point on each segment nearest to the site's location
+        nearestPointsOnSegments = [] #(point index, point distance, streamSegment index)
+
+        #get nearest point in the stream segment
+        for j in range(0, len(potentialLines)):#potential in potentialLines:
+            lineGeom = potentialLines[j].GetGeometryRef()
+            numPoints = lineGeom.GetPointCount()
+
+            nearestPointDist = sys.float_info.max
+            nearestPointIndex = -1
+
+            for i in range(0, numPoints):
+                point = lineGeom.GetPoint(i)
+                dist = math.sqrt((point[0] - sitePoint[0])**2 + (point[1] - sitePoint[1])**2)
+                if dist < nearestPointDist:
+                    nearestPointDist = dist
+                    nearestPointIndex = i
+
+            nearestPointsOnSegments.append((nearestPointIndex, nearestPointDist, j))
+
+        nearestPossibleSnaps = sorted(nearestPointsOnSegments, key=lambda point: point[1])
+
+        needsUserConfirmation = False
+
+        nearestDistance = nearestPossibleSnaps[0][1]
+        verificationDistance = nearestDistance + snapVerificationTolerance * nearestDistance
+        
+        print(nearestPointsOnSegments[0:3])
+
         lineLayer.SetSpatialFilter(None)
+        siteInd = 0
 
 def visualize (gdalData):
     siteLayer = gdalData.siteLayer
     lineLayer = gdalData.lineLayer
     
+    """ siteLayer.ResetReading()
+    feat = siteLayer.GetNextFeature()
+    testBuff = feat.GetGeometryRef().Buffer(3000)
+    lineLayer.SetSpatialFilter(testBuff)
+ """
 
     lineLayer.ResetReading()
     for line in lineLayer:
@@ -70,4 +132,10 @@ attempts = 3
 gdalData.loadFromQuery(y, x, attempts)
 #gdalData.loadFromData()
 Snap(gdalData)
-visualize(gdalData)
+#visualize(gdalData)
+gdalData.siteLayer.ResetReading()
+#stationName_index = gdalData.siteLayer.GetLayerDefn().GetFieldIndex("station_nm")
+
+""" for site in gdalData.siteLayer:
+    stationName = site.GetFieldAsString(stationName_index)
+    print(stationName + " ------- " + getSiteStreamName(stationName)) """
